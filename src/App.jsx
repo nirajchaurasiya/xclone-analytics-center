d5b868777e28dc32961d4c7f10152e0e93524b19
Niraj <developer.nirajchaurasiya@gmail.com>
2024-01-01 15:23:57 +0545

    development text updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ export default function App() {
   return (
     <RoutesElem.Router>
       <RoutesElem.Header />
-      {/* <RoutesElem.Routes>
-            <RoutesElem.Route  />
-        </RoutesElem.Routes> */}
+      <RoutesElem.Routes>
+        <RoutesElem.Route
+          exact
+          path="/"
+          element={
+            <div className="in-development-mode">
+              <div>
+                <h4>This page is in development!!</h4>
+                <p>A tweet will be posted when this page is complete.</p>
+              </div>
+            </div>
+          }
+        />
+      </RoutesElem.Routes>
     </RoutesElem.Router>
   );
 }
